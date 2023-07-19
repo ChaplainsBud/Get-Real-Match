23a3f7b862cf27aa70e261256ab00498c836e4b3
Matt Leroe <matthew.robert.leroe@gmail.com>
2023-07-18 21:42:27 -0400

    Initial Commit

---
+++ get_real_matcher/get_real_matcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  get_real_matcherApp.swift
+//  get_real_matcher
+//
+//  Created by Matthew Leroe on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct get_real_matcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
